769ad98cfb6ea7fa7f31dc023f6fa2511056c6ee
Manuel Stoilov <mstoi002@fiu.edu>
2016-08-30 23:19:23 +0000

    Installed and required body-parser. Added in user, comment, and post schemas and models in app.js

--- app.js
+++ app.js
@@ -1,6 +1,58 @@
 var express = require("express"),
-    app     = express();
+    app     = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    mongo = require("mongodb");
     
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+    
+var userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+var User = mongoose.model("User", userSchema);
+    
+
+var commentSchema = new mongoose.Schema({
+   text: String,
+   author: {
+       id: {
+           type: mongoose.Schema.Types.ObjectId,
+           ref: "User"
+       },
+       username: String
+   }
+});
+
+var Comment = mongoose.model("Comment", commentSchema);
+
+
+
+var postSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
+    comments: [
+        {
+            types: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    
+    ]
+});
+        
+var Post = mongoose.model("Post", postSchema);
+
 
 app.set("view engine", "ejs");
 
