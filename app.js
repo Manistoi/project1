5ac36ed5e54526e4bc62ec34a25638e22caf0854
Manuel Stoilov <mstoi002@fiu.edu>
2016-09-09 13:50:39 +0000

    Created post schema/model. Fixed index get and post routes. Created public directory and added header and footer to files

--- app.js
+++ app.js
@@ -5,20 +5,45 @@ var express     = require("express"),
     
 
 mongoose.connect("mongodb://localhost/project1"); //will try to find project1 db - Won't find it, thus will create it
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended: true}));
+
+var postSchema = new mongoose.Schema({
+   title: String,
+   content: String,
+   date: {type: Date, default: Date.now }
+});
+var Post = mongoose.model("Post", postSchema);
 
 
 app.get("/", function(req, res){
-   res.render("index"); 
+   Post.find({}, function(err, allPosts){
+       if(err) {
+           console.log(err);
+       }
+       else {
+           res.render("index", {posts: allPosts}); 
+       }
+   })
 });
 
 app.get("/posts", function(req, res){
    res.render("posts"); 
 });
 
-app.post("/posts", function(req, res){
+app.post("/", function(req, res){
+    var title = req.body.title;
+    var content = req.body.content;
+    var newPost = {title: title, content: content};
+    Post.create(newPost, function(err, newlyCreated){
+       if(err) {
+           console.log("err");
+       } 
+       else {
+           res.redirect("/");
+       }
+    });
     
 });
 
